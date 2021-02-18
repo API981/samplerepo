5e21589f20e30a8c51475c80efd73bc9a1261ba2
shubham <shubham.pol@apisero.com>
2021-02-19 02:35:14 +0530

    msg 1

--- scrpt.sh
+++ scrpt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Memory information"
 touch test.txt
-echo "While I thought that I was learning how to live, I have been learning how to die." > test.txt
+echo "This better works..!!" > test.txt
