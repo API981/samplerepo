3cf9959872b2c9b18adb407c6d278cf526d00e9c
shubham <shubham.pol@apisero.com>
2021-02-19 13:31:25 +0530

    work

--- scrpt.sh
+++ scrpt.sh
@@ -1,4 +1,25 @@
-#!/bin/bash
-echo "Memory information"
-touch test.txt
-echo "This better works..!!" > test.txt
+#!/bin/sh
+# Program for Fibonacci 
+# Series 
+
+# Static input fo N 
+N=8
+
+# First Number of the 
+# Fibonacci Series 
+a=0 
+
+# Second Number of the 
+# Fibonacci Series 
+b=1 
+echo "$1"
+echo "The Fibonacci series is : "
+
+for (( i=0; i<N; i++ )) 
+do
+	echo -n "$a "
+	fn=$((a + b)) 
+	a=$b 
+	b=$fn 
+done
+# End of for loop 
